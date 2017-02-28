c70b72432718bb47aea497e2282157f2ca78559c
Greg Korthuis <gpkort@gmail.com>
2017-02-27 18:10:49 -0700

    added calss

--- Game/Utilities.py
+++ Game/Utilities.py
@@ -1,5 +1,4 @@
 from Game import Dice
-from enum import Enum
 
 
 def roll_attribute_dice(dicard_threshhold=0):
@@ -15,11 +14,3 @@ def roll_attribute_dice(dicard_threshhold=0):
         die3 = Dice.six_sided()
 
     return die1 + die2 + die3
-
-
-class ATTACK_TYPES(Enum):
-    DEATH_RAY_POISON = 0,
-    MAGIC_WANDS = 1,
-    PARALYSIS_STONE = 2,
-    DRAGON_BREATH = 3,
-    RODS_STAFF_SPELLS = 4
